6e57605c2376f5d352e7cc28aac23ebf6357c088
Pablo Molina <contact@pablomolina.me>
2021-09-09 08:46:48 +0200

    feat: Check out pull request url

--- src/commands/checkout-pull-request.ts
+++ src/commands/checkout-pull-request.ts
@@ -2,26 +2,54 @@ import { defineCommand } from '../define-command';
 import { VerboseGitHubService } from '../services/VerboseGitHubService';
 import { VerboseGitService } from '../services/VerboseGitService';
 
+const PULL_REQUEST_REGEXP =
+  /https:\/\/github.com\/([a-z0-9-_]+)\/([a-z0-9-_]+)\/pull\/([0-9]+)/i;
+
 export const checkoutPullRequest = defineCommand({
-  command: 'checkout-pull-request <number>',
+  command: 'checkout-pull-request <number|url>',
   alias: 'c',
   description: 'checkout specified PR',
   options: [
     {
       definition: '-o, --owner <username>',
-      description: 'repo owner (default: username from "origin")',
+      description:
+        'repo owner, only valid for PR number (default: username from "origin")',
     },
   ],
-  handler: async (numberString: string, options: { owner?: string }) => {
-    const number = parseInt(numberString, 10);
+  handler: async (numberOrURL: string, options: { owner?: string }) => {
+    let repo = '';
+    let owner = '';
+    let number = NaN;
 
-    if (isNaN(number)) {
-      throw new Error(`Invalid PR number "${numberString}"`);
+    const localRepo = await VerboseGitService.getRepositoryName();
+    const localOwner = await VerboseGitService.getRepositoryOwner();
+
+    if (PULL_REQUEST_REGEXP.test(numberOrURL)) {
+      const [, urlOwner, urlRepo, urlNumber] =
+        PULL_REQUEST_REGEXP.exec(numberOrURL) || [];
+
+      if (options.owner && options.owner !== urlOwner) {
+        throw new Error('Cannot use both PR url and owner option');
+      }
+
+      if (localRepo !== urlRepo) {
+        throw new Error(
+          `You are trying to fetch PR into a different project ("${urlRepo}" vs "${localRepo}")`,
+        );
+      }
+
+      repo = urlRepo;
+      owner = urlOwner;
+      number = parseInt(urlNumber, 10);
+    } else {
+      repo = localRepo;
+      owner = options.owner || localOwner;
+      number = parseInt(numberOrURL, 10);
     }
 
-    const repo = await VerboseGitService.getRepositoryName();
-    const owner =
-      options.owner || (await VerboseGitService.getRepositoryOwner());
+    if (isNaN(number)) {
+      throw new Error('Invalid PR number');
+    }
 
     const pr = await VerboseGitHubService.getPullRequest(owner, repo, number);
 
